24175d3034d6bf590b964ab1713416265943e648
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-02 18:52:42 +0200

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -9,27 +9,27 @@ import System.IO (BufferMode (..), hSetBuffering, stdout)
 
 main :: IO ()
 main = do
-    hSetBuffering stdout LineBuffering
+  hSetBuffering stdout LineBuffering
 
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    BC.putStrLn "Logs from your program will appear here"
+  -- You can use print statements as follows for debugging, they'll be visible when running tests.
+  BC.putStrLn "Logs from your program will appear here"
 
-    let host = "127.0.0.1"
-        port = "4221"
-    
-    BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
-    
-    -- Get address information for the given host and port
-    addrInfo <- getAddrInfo Nothing (Just host) (Just port)
-    
-    serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
-    bind serverSocket $ addrAddress $ head addrInfo
-    listen serverSocket 5
-    
-    -- Accept connections and handle them forever
-    forever $ do
-        (clientSocket, clientAddr) <- accept serverSocket
-        BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-        -- Handle the clientSocket as needed...
-    
-        close clientSocket
+  let host = "127.0.0.1"
+      port = "4221"
+
+  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+
+  -- Get address information for the given host and port
+  addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+
+  serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
+  bind serverSocket $ addrAddress $ head addrInfo
+  listen serverSocket 5
+
+  -- Accept connections and handle them forever
+  forever $ do
+    (clientSocket, clientAddr) <- accept serverSocket
+    BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+    -- Handle the clientSocket as needed...
+
+    close clientSocket
