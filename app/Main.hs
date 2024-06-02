87b4fe54d7cf8846233580fabbd2fa0877c7ce50
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-02 20:56:10 +0200

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,28 @@ import qualified Data.ByteString.Char8 as BC
 import Network.Socket
 import System.IO (BufferMode (..), hSetBuffering, stdout)
 import Network.Socket.ByteString (sendAll)
+import System.Log.Logger (rootLoggerName, setLevel, updateGlobalLogger, Priority (INFO), setHandlers, infoM, getLogger)
+import System.Log.Handler.Simple (streamHandler)
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter (simpleLogFormatter)
 
 main :: IO ()
 main = do
+  -- Set up logging to stdout with INFO level using hslogger as simple as possible
+  handler <- streamHandler stdout INFO >>= \lh -> return $ setFormatter lh (simpleLogFormatter "[$time : $loggername : $prio] $msg")
+  updateGlobalLogger rootLoggerName (setHandlers [handler])
+  updateGlobalLogger rootLoggerName (setLevel INFO)
+  let log = infoM "Main"
+
   hSetBuffering stdout LineBuffering
 
   -- You can use print statements as follows for debugging, they'll be visible when running tests.
-  BC.putStrLn "Logs from your program will appear here"
+  log "Logs from your program will appear here"
 
   let host = "127.0.0.1"
       port = "4221"
 
-  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+  log $ "Listening on " <> host <> ":" <> port
 
   -- Get address information for the given host and port
   addrInfo <- getAddrInfo Nothing (Just host) (Just port)
@@ -27,10 +37,12 @@ main = do
   bind serverSocket $ addrAddress $ head addrInfo
   listen serverSocket 5
 
+  log "Ready to accept connections."
+
   -- Accept connections and handle them forever
   forever $ do
     (clientSocket, clientAddr) <- accept serverSocket
-    BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+    log $ "Accepted connection from " <> show clientAddr <> "."
     -- Handle the clientSocket as needed...
     sendAll clientSocket "HTTP/1.1 200 OK\r\n\r\n"
 
