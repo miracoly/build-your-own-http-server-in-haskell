bb733ff6382e915c884bf585908df2861ea3d02a
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-13 19:00:19 +0200

    pin nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cc54fb41d13736e92229c21627ea4f22199fee6b.tar.gz") {};
   ghcVersion = "ghc946";  # Corresponds to GHC 9.4.6
   ghcWithPackages = pkgs.haskell.packages.${ghcVersion}.ghcWithPackages (pkgs: with pkgs; [
     haskell-language-server
